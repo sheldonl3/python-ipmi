3ad6dd4812e6da70357227b8dd5c02b6c1bd7db8
sheldonl3 <tssheldonpotter@gmail.com>
2020-11-02 05:28:10 -0800

    优化结构体

--- command/openssion.py
+++ command/openssion.py
@@ -1,40 +1,49 @@
 # encoding: utf-8
 from ctypes import *
+import struct
 from others import *
 
-class Openssion_Req(BigEndianStructure):
-    _pack_ = 1
-    _fields_ = [
-        #(字段名, c类型 )
-        ('step', c_uint8),
-        ('remoteConsoleSessionID', c_uint32),  #424
-        ('managedSystemSessionID', c_uint32),
-    ]
-    
-    def encode(self):
-        return string_at(addressof(self), sizeof(self))
+class Openssion_Req():
+    def __init__(self):
+        self.step=1
+        self.remoteConsoleSessionID=192
+        self.managedSystemSessionID=168
+        self.typeStr='=BII'
 
-    def decode(self, data):
-        memmove(addressof(self), data, sizeof(self))
-        return len(data)
+class Openssion_Res:
+    def __init__(self,stepResponse,rand_console,rand_bmc,len_cert,cert):
+        self.stepResponse=stepResponse
+        self.rand_console=rand_console
+        self.rand_bmc=rand_bmc
+        self.len_cert=len_cert
+        self.cert=cert
+        self.typeStr='=BIIBp'
 
 def openssion(udp_socket):
     try:
         recv_data,addr = udp_socket.recvfrom(1024)  # 1024表示本次接收的最大字节数
         print("from:",addr)
         ss = Openssion_Req()
-        ss.decode(recv_data)
-        print(ss.step,ss.remoteConsoleSessionID,ss.managedSystemSessionID)
-        if ss.step!=1:
-            print("openssion error")
+        recdata = struct.unpack(ss.typeStr,recv_data)
+
+        print(recdata)
+        if recdata[0]!=1:
             raise ConnectError("openssion error:wrong steps")
- 
-        send_data = "nihao"
-        udp_socket.sendto(send_data.encode(),addr)
+
+        s="dsfsdtr\n"
+        send_struct = Openssion_Res(1,234,456,len(s),s)
+        send_data=struct.pack(
+            send_struct.typeStr,
+            send_struct.stepResponse,
+            send_struct.rand_console,
+            send_struct.rand_bmc,
+            send_struct.len_cert,
+            send_struct.cert)
+        udp_socket.sendto(send_data,addr)
         #udp_socket.close()
 
         print("openssion done\n")
-        return ss.remoteConsoleSessionID,ss.managedSystemSessionID
+        return recdata[1],recdata[2]
         
     except ConnectError,e:
         print(e.error)
